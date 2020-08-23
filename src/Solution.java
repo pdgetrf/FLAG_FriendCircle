3d6ddb87cb9407c2f493a06b27c23993703c87e7
Peng Du <rick.peng.du@gmail.com>
2020-08-23 10:07:41 -0700

    add union find solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public static int findCircleNum(int[][] M) {
-        /*
-        add your homework here
-         */
-        return -1; // place holder. remove after homework
+        int size = M.length;
+        UnionSet us = new UnionSet(size);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (M[i][j] == 1) {
+                    us.Union(i, j);
+                }
+            }
+        }
+
+        Set<Integer> result = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            result.add(us.Find(i));
+        }
+        return result.size();
     }
 }
